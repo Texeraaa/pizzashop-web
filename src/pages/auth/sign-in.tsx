1993d114c7c69209cb071b53b3b81542957e2d6e
Pedro Teixeira <pedro.teixeira@gruponsc.com.br>
2024-04-12 03:20:04 -0300

    feat: pagina de login e mudando o tema

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,10 +1,35 @@
+import { Label } from '@radix-ui/react-label'
 import { Helmet } from 'react-helmet-async'
+
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
 
 export function SignIn() {
   return (
     <>
       <Helmet title="Login" />
-      <h1>SignIn</h1>
+      <div className="p-8">
+        <div className="flex w-[320px] flex-col justify-center gap-6">
+          <div className="flex flex-col gap-2 text-center">
+            <h1 className="text-2xl font-semibold tracking-tight">
+              Acessar Painel
+            </h1>
+            <p className="text-sm text-muted-foreground">
+              Aacompanha suas vendas pelo painel do parceiro
+            </p>
+            <form className="space-y-4">
+              <div className="space-y-2">
+                <Label htmlFor="email">Seu Email</Label>
+                <Input id="email" type="email" />
+              </div>
+              <Button className="w-full" type="submit">
+                {' '}
+                Acessar Painel
+              </Button>
+            </form>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
